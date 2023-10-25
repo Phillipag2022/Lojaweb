91394725b6fb6ada52991d11628d019c462cc2ef
Phillip Gerronimo <pacaciogeronimo@gmail.com>
2023-10-24 22:03:32 -0300

    Desenvolvimento do crud de estado

---
+++ backend/src/main/java/com/lojaweb/backend/repository/EstadoRepository.java
@@ -0,0 +1,14 @@
+package com.lojaweb.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import com.lojaweb.backend.model.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado,Long>{
+
+    
+    
+}
