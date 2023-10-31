b16dbd58f90f07de754aaffc7ab4ff279fd74c9a
Phillip Gerronimo <pacaciogeronimo@gmail.com>
2023-10-30 21:12:59 -0300

    Criaçao do crud de pessoa

---
+++ backend/src/main/java/com/lojaweb/backend/repository/PessoaRepository.java
@@ -0,0 +1,10 @@
+package com.lojaweb.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.lojaweb.backend.model.Pessoa;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa,Long> {
+    
+}
